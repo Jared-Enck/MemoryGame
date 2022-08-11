4d9776c3d6989fe6a121fe56bbf44c7ef417117e
Jared Enck <jared@DESKTOP-GRBT38R.localdomain>
2022-08-11 13:00:47 -0400

    add all the things

---
+++ localScorage.js
@@ -0,0 +1,58 @@
+let currScore = 0;
+let bestScore = 10000;
+let matches = 0;
+
+const resolve = function () {
+  if (cardFlip2) {
+    setTimeout(function () {
+      if (card1.classList.value !== card2.classList.value) {
+        card1.classList.add("facedown");
+        card2.classList.add("facedown");
+        currScore += 10;
+        scoreBarH2.innerText = `Score: ${currScore}`;
+      } else {
+        currScore += 5;
+        scoreBarH2.innerText = `Score: ${currScore}`;
+        matches++;
+      }
+      card1.removeAttribute("id");
+      card2.removeAttribute("id");
+      cardFlip1 = false;
+      cardFlip2 = false;
+      checkGameState();
+      if (gameOver) {
+        endGame();
+      }
+    }, 1200);
+  }
+};
+
+const checkGameState = function () {
+  const totalCards = IMGS.length;
+  if (matches === totalCards / 2) {
+    gameOver = true;
+  }
+};
+
+const endGame = function () {
+  let storedBest = localStorage.getItem("bestScore", JSON.parse(bestScore));
+  if (currScore < storedBest) {
+    bestScore = currScore;
+    scoreBarH3.innerText = `Best Score: ${bestScore}`;
+    localStorage.setItem("bestScore", JSON.stringify(bestScore));
+  }
+  const newGame = document.createElement("div");
+  const NGBanner = document.createElement("h2");
+  NGBanner.innerText = `You Scored: ${currScore} points!`;
+  newGame.classList.add("new-game");
+  const NGBttn = document.createElement("button");
+  NGBttn.classList.add("ng-bttn");
+  NGBttn.innerText = "New Game!";
+  menu.appendChild(newGame);
+  newGame.appendChild(NGBanner);
+  newGame.appendChild(NGBttn);
+  NGBttn.addEventListener("click", function () {
+    newGame.remove();
+    location.reload();
+  });
+};
---
+++ script.js
@@ -0,0 +1,82 @@
+const gameContainer = document.getElementById("game");
+const menu = document.querySelector(".menu");
+const startBttn = document.querySelector(".start");
+let scoreBarH2 = document.createElement("h2");
+let scoreBarH3 = document.createElement("h3");
+let gameOver = false;
+
+const IMGS = [
+  "Finn",
+  "IceKing",
+  "Jake",
+  "LSP",
+  "PBM",
+  "BMO",
+  "Finn",
+  "IceKing",
+  "Jake",
+  "LSP",
+  "PBM",
+  "BMO",
+];
+
+function shuffle(array) {
+  let counter = array.length;
+
+  while (counter > 0) {
+    let index = Math.floor(Math.random() * counter);
+    counter--;
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+  return array;
+}
+
+let shuffledImgs = shuffle(IMGS);
+
+const createDivsForImgs = function (ImgArray) {
+  for (let img of ImgArray) {
+    const card = document.createElement("div");
+    card.classList.add(img, "facedown");
+    card.addEventListener("click", handleCardClick);
+    gameContainer.append(card);
+  }
+};
+
+const gameStart = function () {
+  let storedBest = localStorage.getItem("bestScore", JSON.parse(bestScore));
+  bestScore = storedBest;
+  const HUD = document.createElement("div");
+  HUD.classList.add("HUD");
+  scoreBarH2.innerText = `Score: ${currScore}`;
+  scoreBarH3.innerText = `Best Score: ${bestScore}`;
+  menu.append(HUD);
+  HUD.appendChild(scoreBarH2);
+  HUD.appendChild(scoreBarH3);
+};
+
+startBttn.addEventListener("click", function () {
+  gameStart();
+  startBttn.remove();
+  createDivsForImgs(shuffledImgs);
+});
+
+let cardFlip1 = false;
+let cardFlip2 = false;
+
+function handleCardClick(e) {
+  if (!cardFlip1 && e.target.classList.contains("facedown")) {
+    let card1 = e.target;
+    e.target.classList.remove("facedown");
+    card1.setAttribute("id", "card1");
+    cardFlip1 = true;
+  }
+  if (cardFlip1 && !cardFlip2 && e.target.classList.contains("facedown")) {
+    let card2 = e.target;
+    e.target.classList.remove("facedown");
+    card2.setAttribute("id", "card2");
+    cardFlip2 = true;
+  }
+  resolve();
+}
